940e5a2fda57b1b65151aeb494da0a7161ae63f0
zktm9903 <zktm9903@naver.com>
2021-03-16 22:21:38 +0900

    hihi

---
+++ app/src/main/java/com/example/talkpractice/FriendsListView.java
@@ -0,0 +1,14 @@
+package com.example.talkpractice;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FriendsListView extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_friends_list_view);
+    }
+}
