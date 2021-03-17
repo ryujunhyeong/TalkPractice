f934a7a13788c383d1bd8c3b9320aeec96d32c34
ryujunhyeong <junhyeong0918@gmail.com>
2021-03-17 14:49:36 +0900

    main View clear

--- app/src/main/java/com/example/talkpractice/MainActivity.java
+++ app/src/main/java/com/example/talkpractice/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.view.Window;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
@@ -26,6 +27,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        supportRequestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main);
         mAuth = FirebaseAuth.getInstance();
         etId=(EditText)findViewById(R.id.etId);
